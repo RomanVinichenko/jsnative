a42030ebdf50db042763302963e7158f723bf5df
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-03 17:43:06 +0300

    3

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque blanditiis cumque dicta dolore, ea eius
+      est explicabo fugit impedit incidunt, labore libero nam non nulla recusandae ut, velit veniam!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi culpa dolorem eligendi enim eos
+      expedita facilis maiores minus molestias non provident quas quisquam quos recusandae sequi sit temporibus, velit.
+      expedita facilis maiores minus molestias non provident quas quisquam quos recusandae sequi sit temporibus, velit.
+
+    </div>
+  );
+}
+
+export default App;
